d23685fc5ebb1a75df386b39fdc09a3ac81a4696
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-06-12 08:59:35 +0100

    auth page

---
+++ app/auth.tsx
@@ -0,0 +1,17 @@
+import { KeyboardAvoidingView, Platform, Text, View } from "react-native"
+
+const AuthScreen = () => {
+    return (
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
+            <View>
+                <Text>
+                    {" "}
+                    Create Account
+                </Text>
+            </View>
+
+        </KeyboardAvoidingView>
+    )
+}
+
+export default AuthScreen
