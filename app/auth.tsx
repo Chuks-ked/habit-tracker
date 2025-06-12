b73178785ae32f0a62708ce48b67a3ff146f964a
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-06-12 09:23:10 +0100

    Textinputs and switchmode implemented

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,13 +1,39 @@
-import { KeyboardAvoidingView, Platform, Text, View } from "react-native"
+import { useState } from "react";
+import { KeyboardAvoidingView, Platform, View } from "react-native";
+import { Button, Text, TextInput } from "react-native-paper";
 
 const AuthScreen = () => {
+    const [isSignUp, setIsSignUp] = useState<boolean>(false);
+    const handleSwitchMode = () => {
+        setIsSignUp((prev) => !prev)
+    }
+
     return (
         <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
             <View>
                 <Text>
                     {" "}
-                    Create Account
+                    {isSignUp ? "Create Account" : "Welcome Back"}
                 </Text>
+                <TextInput
+                    label="Email"
+                    autoCapitalize="none"
+                    keyboardType="email-address"
+                    placeholder="example@gmail.com"
+                    mode="outlined"
+                />
+                <TextInput
+                    label="Password"
+                    autoCapitalize="none"
+                    keyboardType="email-address"
+                    mode="outlined"
+                />
+                <Button mode="contained">
+                    {isSignUp ? "Sign Up" : "Sign In"}
+                </Button>
+                <Button mode="text" onPress={handleSwitchMode}>
+                    {isSignUp ? "Already have an account? Sign In" : "Don't have an account? Sign Up"}
+                </Button>
             </View>
 
         </KeyboardAvoidingView>
